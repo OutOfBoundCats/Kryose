b8f6415114253898c3e21cb6c2580cafa67055b1
Raj Patil <floatraj@gmail.com>
2020-06-07 15:31:44 +0530

    Added functionality in api request to extract username so as to cut points from account

--- src/main/java/com/kryose/kryose/Controller/ApiServices.java
+++ src/main/java/com/kryose/kryose/Controller/ApiServices.java
@@ -1,6 +1,9 @@
 package com.kryose.kryose.Controller;
 
 
+import com.kryose.kryose.Util.JwtUtil;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -11,24 +14,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import org.slf4j.Logger;
 
 @RestController
 @RequestMapping("/api")
 public class ApiServices {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    Logger logger= LoggerFactory.getLogger(ApiServices.class);
 
     @PostMapping(value = "/ml_process" , consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Object> ml_process(@RequestParam("file") MultipartFile file) throws IOException {
-        File convertFile = new File(file.getOriginalFilename());
+    public ResponseEntity<Object> ml_process(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
+        //get info from auth header so as to cut points from user account
+        String username = null;
+        String jwt = null;
+        final String authorizationHeader = request.getHeader("Authorization");
+        jwt = authorizationHeader.substring(7);
+        username = jwtUtil.extractUsername(jwt);
+        logger.error(username);
+        //END
+
+        File convertFile = new File("./image/"+file.getOriginalFilename());
         convertFile.createNewFile();
         FileOutputStream fout = new FileOutputStream(convertFile);
         fout.write(file.getBytes());
         fout.close();
 
-        String filename = file.getOriginalFilename();
+        String filename = "./image/"+file.getOriginalFilename();
         File file1 = new File(filename);
         InputStreamResource resource = new InputStreamResource(new FileInputStream(file1));
         HttpHeaders headers = new HttpHeaders();
