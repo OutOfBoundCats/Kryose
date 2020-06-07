0e0784700dfd67c2dbfa139fab207734e7bdb883
Raj Patil <floatraj@gmail.com>
2020-06-07 15:03:53 +0530

    I am able to recive file sending processed file back is still issue

--- src/main/java/com/kryose/kryose/Controller/ApiServices.java
+++ src/main/java/com/kryose/kryose/Controller/ApiServices.java
@@ -1,14 +1,50 @@
 package com.kryose.kryose.Controller;
 
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 public class ApiServices {
 
+    @PostMapping(value = "/ml_process" , consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Object> ml_process(@RequestParam("file") MultipartFile file) throws IOException {
+        File convertFile = new File(file.getOriginalFilename());
+        convertFile.createNewFile();
+        FileOutputStream fout = new FileOutputStream(convertFile);
+        fout.write(file.getBytes());
+        fout.close();
 
+        String filename = file.getOriginalFilename();
+        File file1 = new File(filename);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file1));
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        ResponseEntity<Object>
+                responseEntity = ResponseEntity.ok().headers(headers).contentLength(file1.length()).contentType(
+                MediaType.parseMediaType("application/txt")).body(resource);
+
+        return responseEntity;
+
+    }
 
 
 }
---
+++ src/main/java/com/kryose/kryose/Service/RestService.java
@@ -0,0 +1,17 @@
+package com.kryose.kryose.Service;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RestService {
+
+    private final RestTemplate restTemplate=new RestTemplate();
+
+
+
+
+
+
+}
